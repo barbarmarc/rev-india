dff0bc3ffdb223d0c9b89da1d0d3c788c1f247d8
Marc Barbar <37882990+barbarmarc@users.noreply.github.com>
2020-11-08 17:15:31 -0500

    Update rev_cl.py

--- rev_cl.py
+++ rev_cl.py
@@ -13,43 +13,73 @@ from create_json import *
 SOLAR = "K:/Data/NREL/NSRDB/nsrdb_india_2014.h5"
 WIND = "K:/Data/NREL/WTK/wtk_india_2014.h5"
 
-res_file = WIND
-renewable = 'windpower'
-points_path = 'genx/points/hr_sampling/wind/ER_points.csv'
-points_df = pd.read_csv(points_path)
+renewable_choice = input("Enter renewable resource: ")
+region = input("Enter a region: ")
 
-sam_config = create_g126_84(0)
-#sam_config = create_solar(points_df.timezone.unique()[0])
-write_json(sam_config)
-sam_file = os.path.expanduser('genx/json/sam_config.json')
+if renewable_choice == 'solar':
+	res_file = SOLAR
+	renewable = 'pvwattsv7'
+	points_path = 'genx/points/hr_sampling/solar/'+region+'_points.csv'
+	points_df = pd.read_csv(points_path)
+
+	sam_config = create_solar(points_df.timezone.unique()[0])
+	write_json(sam_config, 'solar', region)
+	sam_file = os.path.expanduser('genx/json/sam_config_'+'solar'+'_'+region+'.json')
+else:
+	res_file = WIND
+	renewable = 'windpower'
+	points_path = 'genx/points/hr_sampling/wind/'+region+'_points.csv'
+	points_df = pd.read_csv(points_path)
+
+	sam_config = create_g126_84(0)
+	write_json(sam_config, 'g126_84', region)
+	sam_file = os.path.expanduser('genx/json/sam_config_'+'g126_84'+'_'+region+'.json')
 
 print(renewable, points_path)
 
-for i in range(4):
+def run_rev(df, res_file, sam_file, renewable, j):
+	lat_lon = []
+	for index, row in df.iterrows():
+		lat_lon.append([row.latitude, row.longitude])
 
-    df = points_df.loc[500*i:500*i+499]
+	pp = ProjectPoints.lat_lon_coords(lat_lon, res_file, sam_file, tech=renewable)
+	pc = PointsControl(pp, sites_per_split=1)
+	gen = Gen.reV_run(tech=renewable, points=pc, sam_files=sam_file,
+						res_file=res_file, max_workers=1, fout=None,
+						output_request=("cf_mean","cf_profile"))
 
-    lat_lon = []
-    for index, row in df.iterrows():
-        lat_lon.append([row.latitude, row.longitude])
+	profile_df = pd.DataFrame(gen.out['cf_profile'])
 
-    pp = ProjectPoints.lat_lon_coords(lat_lon, res_file, sam_file, tech=renewable)
-    pc = PointsControl(pp, sites_per_split=1)
-    gen = Gen.reV_run(tech=renewable, points=pc, sam_files=sam_file,
-                        res_file=res_file, max_workers=1, fout=None,
-                        output_request=("cf_mean","cf_profile"))
+	if renewable == 'windpower':
+		result_df = pd.DataFrame()
+		for col, item in profile_df.iteritems():
+			mean_wind = []
+			for i in range(8760):
+				lst = item.loc[i*12:i*12+11]
+				mean_wind.append(lst.mean()) 
+			result_df[col] = mean_wind
+	else:
+		result_df = profile_df
 
-    profile_df = pd.DataFrame(gen.out['cf_profile'])
+	result_df.to_csv(points_path[:-4]+'_'+str(j)+'.csv')
+	
+if len(points_df) == 2000:
+	for i in range(4):
 
-    if renewable == 'windpower':
-        result_df = pd.DataFrame()
-        for col, item in profile_df.iteritems():
-            mean_wind = []
-            for i in range(8760):
-                lst = item.loc[i*12:i*12+11]
-                mean_wind.append(lst.mean()[0]) 
-            result_df[col] = mean_wind
-    else:
-        result_df = profile_df
+		df = points_df.loc[500*i:500*i+499]
+		
+		run_rev(df, res_file, sam_file, renewable, i)
 
-    result_df.to_csv(points_path[-4]+str(i)+'.csv')
+else:
+	rangeval = int(len(points_df)/500)+1
+	if rangeval > 1:
+		for i in range(rangeval):
+			if i < rangeval-1:
+				df = points_df.loc[500*i:500*i+499]
+			else:
+				df = points_df.loc[500*i:]
+			run_rev(df, res_file, sam_file, renewable, i)
+	else:
+		df = points_df
+		run_rev(df, res_file, sam_file, renewable, 0)
+		
